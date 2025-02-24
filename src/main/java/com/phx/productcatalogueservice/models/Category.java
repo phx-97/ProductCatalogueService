ffab758430fd4ae7ded2c099054e342e169f06c3
sallu97 <msyf97@gmail.com>
2025-02-25 00:53:46 +0530

    Eager loading

--- src/main/java/com/phx/productcatalogueservice/models/Category.java
+++ src/main/java/com/phx/productcatalogueservice/models/Category.java
@@ -2,6 +2,7 @@ package com.phx.productcatalogueservice.models;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,6 +18,6 @@ public class Category extends BaseModel {
 
     private String description;
 
-    @OneToMany(mappedBy = "category")
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
     private List<Product> products;
 }
--- src/test/java/com/phx/productcatalogueservice/repos/CategoryRepoTest.java
+++ src/test/java/com/phx/productcatalogueservice/repos/CategoryRepoTest.java
@@ -30,7 +30,17 @@ class CategoryRepoTest {
 
             Hibernate: select c1_0.id,c1_0.created_at,c1_0.description,c1_0.last_updated_at,c1_0.name,c1_0.status from category c1_0 where c1_0.id=?
             Hibernate: select p1_0.category_id,p1_0.id,p1_0.created_at,p1_0.description,p1_0.image_url,p1_0.last_updated_at,p1_0.name,p1_0.price,p1_0.status from product p1_0 where p1_0.category_id=?
-P
+
+            -------------------
+            In case of Eager loading -- only single query will be printed and complete data of Category and it's associated tables will be fetched in a single go
+
+            To explicitly define the fetch type use the below syntax in Category model --
+            @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+            private List<Product> products;
+
+            Query:
+                Hibernate: select c1_0.id,c1_0.created_at,c1_0.description,c1_0.last_updated_at,c1_0.name,c1_0.status,p1_0.category_id,p1_0.id,p1_0.created_at,p1_0.description,p1_0.image_url,p1_0.last_updated_at,p1_0.name,p1_0.price,p1_0.status from category c1_0 left join product p1_0 on c1_0.id=p1_0.category_id where c1_0.id=?
+
          */
     }
 
